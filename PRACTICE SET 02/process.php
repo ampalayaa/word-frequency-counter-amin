f91ec897de8b820f776324b54769b3b07adf292e
Kenjicci <Cincokencstd1@gmail.com>
2024-09-22 16:39:08 +0800

    revised the cleaning text

--- PRACTICE SET 02/process.php
+++ PRACTICE SET 02/process.php
@@ -7,15 +7,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // functions
     //cleaning text
     function clean_text($text) {
+        $text = preg_replace('/[\r\n]+/', ' ', $text); 
+        $text = preg_replace('/\s+/', ' ', $text); // papalitan ng single space ang multiple space
         $remove_special_char = preg_replace('/[^a-zA-Z\s]/', '', $text);
         $articles = ['the', 'a', 'an', 'for', 'and', 'nor', 'but', 'or', 'yet', 'so', 'is', 'are', 'am']; // array of words to be ignored
-        $pattern = '/\b(' . implode('|', $articles) . ')\b/i'; // the regex pattern for articles, commmon verb, and conjuctions    
-        $remove_articles = str_ireplace($pattern, '', $remove_special_char);
-        $lowered_txt = strtolower($remove_articles);
-        $words = explode(' ', $lowered_txt);
-        return $words;
+        $pattern = '/\b(' . implode('|', $articles) . ')\b/i'; // regex pattern for articles, common verbs, and conjunctions    
+        $remove_articles = preg_replace($pattern, '', $remove_special_char);
+        $lowered_txt = strtolower(trim($remove_articles));
+        $cleaned = explode(' ', $lowered_txt);
+        
+        return array_filter($cleaned); // return non-empty values
     }
-
+    
+    $words = clean_text($text);
+    print_r($words);
 };
-
 ?>
